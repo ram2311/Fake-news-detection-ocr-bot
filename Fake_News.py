d68d3a4eb078784799819aa6308ded89c3b7c97b
ram2311 <68749260+ram2311@users.noreply.github.com>
2021-07-09 22:48:04 +0530

    Update Fake_News.py

--- Fake_News.py
+++ Fake_News.py
@@ -105,7 +105,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://Fake-News-detectable-ocr-bot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://ocr-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
